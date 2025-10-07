64d3835509c3c689a4ff520d1a1812433947495f
Jake Berryman <dragons4me@comcast.net>
2025-10-07 12:04:59 -0500

    create button and listener, fix main layour

--- app/src/main/java/com/example/tasktracker/BottomFragment.java
+++ app/src/main/java/com/example/tasktracker/BottomFragment.java
@@ -1,6 +1,30 @@
 package com.example.tasktracker;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class BottomFragment extends Fragment {
+    public BottomFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_fragment, container, false);
+
+
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/tasktracker/TopFragment.java
+++ app/src/main/java/com/example/tasktracker/TopFragment.java
@@ -1,6 +1,37 @@
 package com.example.tasktracker;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
 
 public class TopFragment extends Fragment {
+    public TopFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_fragment, container, false);
+
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+    }
 }
